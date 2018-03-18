ff9ad90ea37c95a6bdff629e8238a883cbe0cf54
Joshua Nielsen <josh.w.nielsen@gmail.com>
2018-03-18 13:52:54 -0600

    oAuth almost done

--- MyFullStack/server/services/passport.js
+++ MyFullStack/server/services/passport.js
@@ -1,6 +1,20 @@
 const keys = require('../config/keys');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 
 passport.use(
   new GoogleStrategy(
@@ -11,8 +25,16 @@ passport.use(
       proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken: ', accessToken);
-      // console.log('refreshToken: ', refreshToken);
-      console.log('profile: ', profile);
+      User.findOne({ googleId: profile.id }).then( existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => {
+              done(null, user);
+          });
+        }
+      });
     }
   ));
