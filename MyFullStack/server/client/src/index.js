5e32f83a97c178dc1db10afd8f64553269baebf0
Joshua Nielsen <josh.w.nielsen@gmail.com>
2018-03-19 23:11:14 -0600

    Fixed react-form error

--- MyFullStack/server/client/src/index.js
+++ MyFullStack/server/client/src/index.js
@@ -6,13 +6,13 @@ import { createStore, applyMiddleware } from 'redux';
 // import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
-// import reducers from './reducers';
+import reducers from './reducers';
 
 // Development only axios helpers!
 // import axios from 'axios';
 // window.axios = axios;
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
 
 // const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
