674a6988cf441038a0b87fb1f7873110eaf2678c
Joshua Nielsen <josh.w.nielsen@gmail.com>
2018-03-20 20:41:36 -0600

    React landing is started

--- MyFullStack/server/client/src/components/App.js
+++ MyFullStack/server/client/src/components/App.js
@@ -1,44 +1,21 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-// import { connect } from 'react-redux';
-// import * as actions from '../actions';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-// import Header from './Header';
+import Header from './Header';
 // import Landing from './Landing';
 // import Dashboard from './Dashboard';
 // import SurveyNew from './surveys/SurveyNew';
 
-// class App extends Component {
-//   componentDidMount() {
-//     this.props.fetchUser();
-//   }
 
-//   render() {
-//     return (
-//       <div className="container">
-//         <BrowserRouter>
-//           <div>
-//             <Header />
-//             <Route exact path="/" component={Landing} />
-//             <Route exact path="/surveys" component={Dashboard} />
-//             <Route path="/surveys/new" component={SurveyNew} />
-//           </div>
-//         </BrowserRouter>
-//       </div>
-//     );
-//   }
-// }
-
-// export default connect(null, actions)(App);
-
-const Header = () => <h2>Header</h2>
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
 class App extends Component {
   componentDidMount() {
-    // this.props.fetchUser();
+    this.props.fetchUser();
   }
 
   render() {
@@ -57,4 +34,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(null, actions)(App);
--- MyFullStack/server/client/src/index.js
+++ MyFullStack/server/client/src/index.js
@@ -1,9 +1,9 @@
-// import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-// import reduxThunk from 'redux-thunk';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
@@ -12,9 +12,9 @@ import reducers from './reducers';
 // import axios from 'axios';
 // window.axios = axios;
 
-const store = createStore(reducers, {}, applyMiddleware());
+// const store = createStore(reducers, {}, applyMiddleware());
 
-// const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
