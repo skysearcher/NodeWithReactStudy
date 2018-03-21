7cb54b4852e857a46d2fc7dfcb0ee8844d576be9
Joshua Nielsen <josh.w.nielsen@gmail.com>
2018-03-20 22:17:10 -0600

    Payment form is created and should be done

--- MyFullStack/server/client/src/components/Header.js
+++ MyFullStack/server/client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
   renderContent() {
@@ -11,7 +12,7 @@ class Header extends Component {
         return <li><a href="/auth/google">Login With Google</a></li>;
       default:
         return [
-          // <li key="1"><Payments /></li>,
+          <li key="1"><Payments /></li>,
           <li key="3" style={{ margin: '0 10px' }}>
             Credits: {this.props.auth.credits}
           </li>,
@@ -24,9 +25,11 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
+            className="left brand-logo">
             Emaily
-          </a>
+          </Link>
           <ul className="right">
             {this.renderContent()}
           </ul>
@@ -41,50 +44,3 @@ function mapStateToProps({ auth }) {
 }
 
 export default connect(mapStateToProps)(Header);
-
-// import { connect } from 'react-redux';
-// import { Link } from 'react-router-dom';
-// import Payments from './Payments';
-
-// class Header extends Component {
-//   renderContent() {
-//     switch (this.props.auth) {
-//       case null:
-//         return;
-//       case false:
-//         return <li><a href="/auth/google">Login With Google</a></li>;
-//       default:
-//         return [
-//           <li key="1"><Payments /></li>,
-//           <li key="3" style={{ margin: '0 10px' }}>
-//             Credits: {this.props.auth.credits}
-//           </li>,
-//           <li key="2"><a href="/api/logout">Logout</a></li>
-//         ];
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <nav>
-//         <div className="nav-wrapper">
-//           <Link
-//             to={this.props.auth ? '/surveys' : '/'}
-//             className="left brand-logo"
-//           >
-//             Emaily
-//           </Link>
-//           <ul className="right">
-//             {this.renderContent()}
-//           </ul>
-//         </div>
-//       </nav>
-//     );
-//   }
-// }
-
-// function mapStateToProps({ auth }) {
-//   return { auth };
-// }
-
-// export default connect(mapStateToProps)(Header);
